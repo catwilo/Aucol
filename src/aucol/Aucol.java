52c716f90b9acf8297f5561d37adda42a6e04deb
catwilo <31023673+catwilo@users.noreply.github.com>
2020-04-24 16:09:51 -0500

    Initial commit

---
+++ src/aucol/Aucol.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aucol;
+
+/**
+ *
+ * @author cattwinlow
+ */
+public class Aucol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
