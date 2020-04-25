bcf5ea4f1d034912f3361a6b05ec12800c1d4fdb
cpadillas <30636268+cpadillas@users.noreply.github.com>
2020-04-25 11:36:18 -0500

    Se agregan las estructuras genericas

---
+++ src/Structures/Lista.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Structures;
+
+/**
+ *
+ * @author crist
+ */
+public class Lista<T extends Comparable> {
+    private Nodo_Doble<T> head;
+
+    public Lista(){
+        head = null;
+    }
+
+    public boolean insert(T item){
+
+        boolean inserted;
+        Nodo_Doble<T> ptr, NodeI;
+        inserted = false;
+        ptr = head;
+        while(ptr != null && ptr.getNext() != null && !ptr.getData().equals(item)){
+            ptr = ptr.getNext();
+        }
+        Nodo_Doble newi = new Nodo_Doble(item);
+        if(ptr == null){
+            inserted = true;
+            head = newi;
+        }else{
+            if(!ptr.getData().equals(item)){
+                inserted = true;
+                ptr.setNext(newi);
+                newi.setBack(ptr);
+            }
+        }
+
+        return  inserted;
+    }
+
+    public boolean delete(T item){
+
+        boolean deleted;
+        Nodo_Doble<T> ptr;
+        deleted = false;
+        ptr = head;
+
+        while(ptr != null && !ptr.getData().equals(item)){
+            ptr = ptr.getNext();
+        }
+        if(ptr != null){
+            deleted = true;
+            if(ptr.getBack() != null && ptr.getNext() != null){
+                ptr.getBack().setNext(ptr.getNext());
+                ptr.getNext().setBack(ptr.getBack());
+            }else if(ptr.getBack() == null && ptr.getNext() != null){
+                head = ptr.getNext();
+                ptr.getNext().setBack(null);
+            }else if(ptr.getNext() == null && ptr.getBack() != null){
+                ptr.getBack().setNext(null);
+            }else{
+                head = null;
+            }
+
+        }
+        return deleted;
+    }
+
+
+    public Nodo_Doble<T> getHead() {
+        return head;
+    }
+}
